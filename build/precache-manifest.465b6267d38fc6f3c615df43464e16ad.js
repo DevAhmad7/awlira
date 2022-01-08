97a46caeed05e4831e73fdd016655e6f5f4dc073
Ahmad Hassan <ah77731@gmail.com>
2022-01-08 23:11:18 +0200

    add build files

---
+++ build/precache-manifest.465b6267d38fc6f3c615df43464e16ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2230dfbf0144acb4fb328935cda830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a63f9e479d0f6b9e98",
+    "url": "/static/css/main.3290ff6e.chunk.css"
+  },
+  {
+    "revision": "61746062c39869520db0",
+    "url": "/static/js/2.8413ea12.chunk.js"
+  },
+  {
+    "revision": "66c7236c3751fcb8e7dfb6d8983314fd",
+    "url": "/static/js/2.8413ea12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a63f9e479d0f6b9e98",
+    "url": "/static/js/main.cd3a9161.chunk.js"
+  },
+  {
+    "revision": "fb07f4430935e11f2fd8",
+    "url": "/static/js/runtime-main.3dde5a5d.js"
+  }
+]);
